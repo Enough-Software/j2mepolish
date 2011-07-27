6310518111017740178083d46620ce36bac2dd43
Robert Virkus <robert.virkus@enough.de>
2011-07-27 16:29:26 +0200

    added vertical-doors-screen-change-animation-door CSS attribute

--- enough-polish-j2me/source/src/de/enough/polish/ui/screenanimations/VerticalDoorsScreenChangeAnimation.java
+++ enough-polish-j2me/source/src/de/enough/polish/ui/screenanimations/VerticalDoorsScreenChangeAnimation.java
@@ -47,11 +47,15 @@ public class VerticalDoorsScreenChangeAnimation extends ScreenChangeAnimation
 	private static final int DIRECTION_DEFAULT = 0;
 	private static final int DIRECTION_CLOSE = 1;
 	private static final int DIRECTION_OPEN = 2;
+	private static final int DOOR_BOTH = 0;
+	private static final int DOOR_TOP = 1;
+	private static final int DOOR_BOTTOM = 2;
 	
 	private int currentY;
 	private long duration = 500;
 	private int direction = DIRECTION_DEFAULT;
 	private long startTime;
+	private int door = DOOR_BOTH;
 
 	/**
 	 * Creates a new animation 
@@ -87,6 +91,15 @@ public class VerticalDoorsScreenChangeAnimation extends ScreenChangeAnimation
 				this.direction = DIRECTION_DEFAULT;
 			}
 		//#endif
+		//#if polish.css.vertical-doors-screen-change-animation-door
+			Integer doorInt = style.getIntProperty("vertical-doors-screen-change-animation-door");
+			if (doorInt != null)
+			{
+				this.door = doorInt.intValue();
+			} else {
+				this.door = DOOR_BOTH;
+			}
+		//#endif
 	}
 	
 	
@@ -103,9 +116,9 @@ public class VerticalDoorsScreenChangeAnimation extends ScreenChangeAnimation
 		this.startTime = System.currentTimeMillis();
 		//#if polish.css.vertical-doors-screen-change-animation-direction
 			if (this.direction == DIRECTION_CLOSE) {
-				this.isForwardAnimation = true;
-			} else if (this.direction == DIRECTION_OPEN) {
 				this.isForwardAnimation = false;
+			} else if (this.direction == DIRECTION_OPEN) {
+				this.isForwardAnimation = true;
 			}
 		//#endif
 	}
@@ -146,11 +159,32 @@ public class VerticalDoorsScreenChangeAnimation extends ScreenChangeAnimation
 			second = this.lastCanvasImage;
 			height = this.currentY;
 		}
-
+		// draw last (direction==DIRECTION_CLOSING/isForwardAnimation) or next screen:
 		g.drawImage(first, 0, 0, Graphics.TOP | Graphics.LEFT);
-		g.setClip(0, 0, this.screenWidth, height );
-		g.drawImage(second, 0, height - (this.screenHeight/2), Graphics.TOP | Graphics.LEFT);
-		g.setClip(0, this.screenHeight-height, this.screenWidth, height );
-		g.drawImage(second, 0, (this.screenHeight/2) - height , Graphics.TOP | Graphics.LEFT);
+		// draw top door:
+		//#if polish.css.vertical-doors-screen-change-animation-door
+			if (this.door == DOOR_BOTH || this.door == DOOR_TOP) {
+		//#endif
+				g.setClip(0, 0, this.screenWidth, height );
+				g.drawImage(second, 0, height - (this.screenHeight/2), Graphics.TOP | Graphics.LEFT);
+		//#if polish.css.vertical-doors-screen-change-animation-door
+			} else {
+				g.setClip(0, 0, this.screenWidth, this.screenHeight/2 );
+				g.drawImage(second, 0, 0, Graphics.TOP | Graphics.LEFT);				
+			}
+		//#endif
+			
+		// draw bottom door:
+			//#if polish.css.vertical-doors-screen-change-animation-door
+			if (this.door == DOOR_BOTH || this.door == DOOR_BOTTOM) {
+		//#endif
+				g.setClip(0, this.screenHeight-height, this.screenWidth, height );
+				g.drawImage(second, 0, (this.screenHeight/2) - height , Graphics.TOP | Graphics.LEFT);
+		//#if polish.css.vertical-doors-screen-change-animation-door
+			} else {
+				g.setClip(0, this.screenHeight/2, this.screenWidth, this.screenHeight/2 + 1 );
+				g.drawImage(second, 0, 0, Graphics.TOP | Graphics.LEFT);				
+			}
+		//#endif
 	}
 }
